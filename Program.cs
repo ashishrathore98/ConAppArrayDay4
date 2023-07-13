e0f6dd0498c59ba89d84bab118d36d7939139d45
ashishrathore98 <ashrathore98@gmail.com>
2023-07-13 10:52:26 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] marks = new int[5];
+            //Console.WriteLine("Enter marks:");
+            //for (int i = 0; i < marks.Length; i++)
+            //{
+            //    Console.WriteLine("Enter marks in Subject: ");
+            //    marks[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < marks.Length; i++)
+            //{
+            //    Console.WriteLine($"Subject {i + 1} \t Marks:  {marks[i]}");
+            //}
+            //Console.ReadKey();
+
+            //Console.WriteLine("Enter No of Students: ");
+            //int nos = int.Parse(Console.ReadLine());
+            //string[] students = new string[nos];
+            //for (int i = 0; i < nos; i++)
+            //{
+            //    Console.WriteLine($"Enter Students{i + 1}\'s name");
+            //    students[i] = Console.ReadLine();
+            //}
+            //Console.WriteLine("List of Students");
+            //foreach (string student in students)
+            //{
+            //    Console.WriteLine(student);
+            //}
+            //Console.ReadKey();
+
+            // int[] marks = { 11, 22, 33, 44, 55 };
+            // int[] marks = new int [4] { 11, 12, 12, 14,};
+            //    Console.WriteLine("Marks List: ");
+            //    for(int i = 0; i < marks.Length; i++)
+            //    {
+            //        Console.WriteLine($"Subject {i+1} \t Marks {marks[i]}");
+            //    }
+            //    Console.ReadKey();
+
+            //Console.WriteLine("Enter No of Students: ");
+            //int nos=int.Parse(Console.ReadLine());
+            //string[] students = new string[nos];
+            //for(int i = 0; i < nos; i++)
+            //{
+            //    Console.WriteLine($"Enter Students{i+1}\'s name");
+            //    students[i] = Console.ReadLine();
+            //}
+            //Console.WriteLine("List of Students");
+            //foreach (string student in students)
+            //{
+            //    Console.WriteLine(student);
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
